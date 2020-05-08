b14bf9afa0cc8f474b68d2dabac2f2bec4d83521
shinebar <wangjinshan08@163.com>
2020-05-08 12:34:25 +0800

    api-driver

---
+++ api-driver/src/test/java/api/driver/ApiDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package api.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
